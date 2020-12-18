1ff4f0b911d50959e3ba771a96cb7b2706ad5dc7
Nguyễn Văn Công <congnv3030@gmail.com>
2020-12-18 08:46:36 +0700

    Them bang DatVe

---
+++ HC_QLDatVeXemPhim/Entities/DatVe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HC_QLDatVeXemPhim.Entities
+{
+    public class DatVe
+    {
+        [Key]
+        public int MaDatVe { get; set; }
+
+        [Required]
+        public string SoDienThoai { get; set; }
+
+        [ForeignKey(nameof(LichChieuPhim))]
+        public int MaLichChieuPhim { get; set; }
+
+        public LichChieuPhim LichChieuPhim { get; set; }
+
+        public DateTime ThoiGianDat { get; set; }
+    }
+}
